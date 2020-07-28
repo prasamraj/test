351c23c7f9fb8905733a413c4c396c1b66cd181d
prasamraj <57229820+prasamraj@users.noreply.github.com>
2020-07-28 18:31:47 +0545

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-
+var http = require('http');
+var server = http.createServer(function(request, response) {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("Hello World!");
+});
+var port = 3001;
+server.listen(port);
+console.log("Server running at http://localhost:%d", port);
